07a8ea44f6b15f7acd65278efdadbfe42a15bd27
Zane Riley <zriley@add123.com>
2021-06-09 15:37:42 -0400

    WIP

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import { format, parse, isValid } from 'date-fns'
+
+export const FORMAT_SERVER_DATETIME = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+export const FORMAT_SERVER_DATE = 'yyyy-MM-dd'
+
+export const FORMAT_DISPLAY_DATE = 'M/d/yyyy'
+export const FORMAT_DISPLAY_DATETIME = 'M/d/yyyy h:mm a'
+
+const getUTCDate = date => {
+  return new Date(
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds(),
+    date.getMilliseconds()
+  )
+}
+
+export const getLocalDate = date => {
+  return new Date(Date.UTC(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  ))
+}
+
+export const formatServerDate = dateObj => {
+  if (typeof dateObj === 'string') throw new Error('Input is a string, expected a Date Object')
+  return isValid(dateObj) ? format(dateObj, FORMAT_SERVER_DATE) : ''
+}
+
+export const formatServerDateTime = dateObj => {
+  if (typeof dateObj === 'string') throw new Error('Input is a string, expected a Date Object')
+  return isValid(dateObj) ? format(dateObj, FORMAT_SERVER_DATETIME) : ''
+}
+
+export const formatDisplayDateTime = dateObj => {
+  if (typeof dateObj === 'string') throw new Error('Input is a string, expected a Date Object')
+  return isValid(dateObj) ? format(dateObj, FORMAT_DISPLAY_DATETIME) : ''
+}
+
+export const formatDisplayDate = dateObj => {
+  if (typeof dateObj === 'string') throw new Error('Input is a string, expected a Date Object')
+  return isValid(dateObj) ? format(dateObj, FORMAT_DISPLAY_DATE) : ''
+}
+
+export const parseServerDate = dateString => {
+  if (!dateString) return
+  if (typeof dateString !== 'string') throw new Error('Input is not a string')
+  return getUTCDate(parse(dateString, FORMAT_SERVER_DATE, new Date(0)))
+}
+
+export const parseServerDateTime = dateString => {
+  if (!dateString) return
+  if (typeof dateString !== 'string') throw new Error('Input is not a string')
+  return getLocalDate(parse(dateString, FORMAT_SERVER_DATETIME, new Date(0)))
+}
---
+++ src/index.test.js
@@ -0,0 +1,41 @@
+import { formatServerDate, formatServerDateTime, formatDisplayDateTime, formatDisplayDate } from './dateUtils.js'
+
+const isFunction = x => typeof x === 'function'
+
+describe('dateUtils', () => {
+  describe('formatDisplayDate', () => {
+    it('is a function', () => {
+      expect(isFunction(formatDisplayDate)).toBeTruthy()
+    })
+    it('returns 11/15/2020 when passed new Date(2020, 10, 15)', () => {
+      expect(formatDisplayDate(new Date(2020, 10, 15))).toEqual('11/15/2020')
+    })
+  })
+
+  describe('formatServerDateTime', () => {
+    it('is a function', () => {
+      expect(isFunction(formatServerDateTime)).toBeTruthy()
+    })
+    it('returns 11/15/2020 6:23 AM when passed new Date(2020, 10, 15, 6, 23)', () => {
+      expect(formatServerDateTime(new Date(2020, 10, 15, 6, 23))).toEqual('2020-11-15T06:23:00.000Z')
+    })
+  })
+
+  describe('formatServerDateTime', () => {
+    it('is a function', () => {
+      expect(isFunction(formatDisplayDateTime)).toBeTruthy()
+    })
+    it('returns 11/15/2020 6:23 AM when passed new Date(2020, 10, 15, 6, 23)', () => {
+      expect(formatDisplayDateTime(new Date(2020, 10, 15, 6, 23))).toEqual('11/15/2020 6:23 AM')
+    })
+  })
+
+  describe('formatServerDate', () => {
+    it('is a function', () => {
+      expect(isFunction(formatServerDate)).toBeTruthy()
+    })
+    it('returns 2020-11-15 when passed in new Date(2020, 10, 15)', () => {
+      expect(formatServerDate(new Date(2020, 10, 15))).toEqual('2020-11-15')
+    })
+  })
+})

512f10149114ec5ab13efd0c81157ab855e75e0b
andres <andresdelgado@andresdesk.add123.com>
2021-06-24 08:54:55 -0400

    fixes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { format, parse, isValid } from 'date-fns'
 export const FORMAT_SERVER_DATETIME = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
 export const FORMAT_SERVER_DATE = 'yyyy-MM-dd'
 
-export const FORMAT_DISPLAY_DATE = 'M/d/yyyy'
-export const FORMAT_DISPLAY_DATETIME = 'M/d/yyyy h:mm a'
+export const FORMAT_DISPLAY_DATE = 'MM/dd/yyyy'
+export const FORMAT_DISPLAY_DATETIME = 'MM/dd/yyyy h:mm a'
 
 const getUTCDate = date => {
   return new Date(
@@ -32,35 +32,37 @@ const getLocalDate = date => {
 
 export const formatServerDate = dateObj => {
   if (typeof dateObj === 'string') throw new Error('Input is a string, expected a Date Object')
-  return isValid(dateObj) ? format(dateObj, FORMAT_SERVER_DATE) : ''
+  return isValid(dateObj) ? format(getUTCDate(dateObj), FORMAT_SERVER_DATE) : undefined
 }
 
 export const formatServerDateTime = dateObj => {
   if (typeof dateObj === 'string') throw new Error('Input is a string, expected a Date Object')
-  return isValid(dateObj) ? format(dateObj, FORMAT_SERVER_DATETIME) : ''
+  return isValid(dateObj) ? format(getUTCDate(dateObj), FORMAT_SERVER_DATETIME) : undefined
 }
 
 export const formatDisplayDateTime = dateObj => {
   if (typeof dateObj === 'string') throw new Error('Input is a string, expected a Date Object')
-  return isValid(dateObj) ? format(dateObj, FORMAT_DISPLAY_DATETIME) : ''
+  return isValid(dateObj) ? format(getUTCDate(dateObj), FORMAT_DISPLAY_DATETIME) : ''
 }
 
 export const formatDisplayDate = dateObj => {
   if (typeof dateObj === 'string') throw new Error('Input is a string, expected a Date Object')
-  return isValid(dateObj) ? format(dateObj, FORMAT_DISPLAY_DATE) : ''
+  return isValid(dateObj) ? format(getUTCDate(dateObj), FORMAT_DISPLAY_DATE) : ''
 }
 
 export const parseServerDate = dateString => {
-  if (!dateString) return
+  if (!dateString) return 
   if (typeof dateString !== 'string') throw new Error('Input is not a string')
   dateString = dateString.split(':')[0]
-  return getUTCDate(parse(dateString, FORMAT_SERVER_DATE, new Date(0)))
+  const date = parse(dateString, FORMAT_SERVER_DATE, new Date(0))
+  return isValid(date) ? date : undefined
 }
 
 export const parseServerDateTime = dateString => {
-  if (!dateString) return
+  if (!dateString) return 
   if (typeof dateString !== 'string') throw new Error('Input is not a string')
-  return getLocalDate(parse(dateString, FORMAT_SERVER_DATETIME, new Date(0)))
+  const date = parse(dateString, FORMAT_SERVER_DATETIME, new Date(0))
+  return isValid(date) ? getLocalDate(date) : undefined
 }
 
 export const convertServerDateTime = serverDate => formatDisplayDateTime(parseServerDateTime(serverDate))
--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,9 @@ import { formatServerDate, formatServerDateTime, formatDisplayDateTime, formatDi
 
 const isFunction = x => typeof x === 'function'
 
+const trunc = date =>
+  new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0)
+
 describe('dateUtils', () => {
   describe('formatDisplayDate', () => {
     it('is a function', () => {
@@ -10,14 +13,21 @@ describe('dateUtils', () => {
     it('returns 11/15/2020 when passed new Date(2020, 10, 15)', () => {
       expect(formatDisplayDate(new Date(2020, 10, 15))).toEqual('11/15/2020')
     })
+    it('can format Date display format', () => {
+      expect(formatDisplayDate(new Date(0))).toBe('01/01/1970')
+      expect(formatDisplayDate(new Date(1001))).toBe('01/01/1970')
+      expect(formatDisplayDate(new Date(1605416400000))).toBe('11/15/2020')
+    })
   })
 
   describe('formatServerDateTime', () => {
     it('is a function', () => {
       expect(isFunction(formatServerDateTime)).toBeTruthy()
     })
-    it('returns 11/15/2020 6:23 AM when passed new Date(2020, 10, 15, 6, 23)', () => {
-      expect(formatServerDateTime(new Date(2020, 10, 15, 6, 23))).toEqual('2020-11-15T06:23:00.000Z')
+    it('can format DateTime server format', () => {
+      expect(formatServerDateTime(new Date(0))).toBe('1970-01-01T00:00:00.000Z')
+      expect(formatServerDateTime(new Date(1001))).toEqual('1970-01-01T00:00:01.001Z')
+      expect(formatServerDateTime(new Date(1605416400000))).toEqual('2020-11-15T05:00:00.000Z')
     })
   })
 
@@ -26,7 +36,9 @@ describe('dateUtils', () => {
       expect(isFunction(formatDisplayDateTime)).toBeTruthy()
     })
     it('returns 11/15/2020 6:23 AM when passed new Date(2020, 10, 15, 6, 23)', () => {
-      expect(formatDisplayDateTime(new Date(2020, 10, 15, 6, 23))).toEqual('11/15/2020 6:23 AM')
+      expect(formatDisplayDateTime(new Date(0))).toBe('01/01/1970 12:00 AM')
+      expect(formatDisplayDateTime(new Date(1001))).toEqual('01/01/1970 12:00 AM')
+      expect(formatDisplayDateTime(new Date(1605416400000))).toEqual('11/15/2020 5:00 AM')
     })
   })
 
@@ -37,6 +49,11 @@ describe('dateUtils', () => {
     it('returns 2020-11-15 when passed in new Date(2020, 10, 15)', () => {
       expect(formatServerDate(new Date(2020, 10, 15))).toEqual('2020-11-15')
     })
+    it('can format Date server format', () => {
+      expect(formatServerDate(new Date(0))).toBe('1970-01-01')
+      expect(formatServerDate(new Date(1001))).toBe('1970-01-01')
+      expect(formatServerDate(new Date(2020, 10, 15))).toBe('2020-11-15')
+    })
   })
 
   describe('parseServerDate', () => {
@@ -44,10 +61,22 @@ describe('dateUtils', () => {
       expect(isFunction(parseServerDate)).toBeTruthy()
     })
     it('returns new Date(2020, 10, 15, 5) when passed in 2020-11-15', () => {
-      expect(parseServerDate('2020-11-15')).toEqual(new Date(2020, 10, 15, 5))
+      expect(parseServerDate('2020-11-15')).toEqual(trunc(new Date(2020, 10, 15, 5)))
     })
     it('ignores timezone', () => {
-      expect(parseServerDate('2020-11-15:New_York')).toEqual(new Date(2020, 10, 15, 5))
+      expect(parseServerDate('2020-11-15:New_York')).toEqual(trunc(new Date(2020, 10, 15, 5)))
+    })
+    it('can parse Date server format', () => {
+      expect(formatServerDate(parseServerDate('2020-10-15'))).toEqual('2020-10-15')
+  
+      expect(parseServerDate('2020-10-15')).toEqual(trunc(new Date(2020, 9, 15)))
+      expect(parseServerDate('1970-01-01')).toEqual(trunc(new Date(1970, 0, 1)))
+      expect(parseServerDate('2020-11-15')).toEqual(trunc(new Date(2020, 10, 15)))
+      expect(parseServerDate('2020-10-15')).toEqual(trunc(new Date(2020, 9, 15)))
+    })
+    it('ignores bad input', () => {
+      expect(parseServerDate(undefined)).toEqual(undefined)
+      expect(parseServerDate('24')).toEqual(undefined)
     })
   })
 
@@ -55,8 +84,14 @@ describe('dateUtils', () => {
     it('is a function', () => {
       expect(isFunction(parseServerDateTime)).toBeTruthy()
     })
-    it('returns new Date(2020, 10, 15, 11, 34, 52) when passed in 2020-11-15T16:34:52.000Z', () => {
-      expect(parseServerDateTime('2020-11-15T16:34:52.000Z')).toEqual(new Date(2020, 10, 15, 11, 34, 52))
+    it('can parse DateTime server format', () => {
+      expect(parseServerDateTime('1970-01-01T00:00:00.000Z')).toEqual(new Date(0))
+      expect(parseServerDateTime('1970-01-01T00:00:01.001Z')).toEqual(new Date(1001))
+      expect(parseServerDateTime('2020-11-15T05:00:00.000Z')).toEqual(new Date(1605416400000))
+    })
+    it('ignores bad input', () => {
+      expect(parseServerDate(undefined)).toEqual(undefined)
+      expect(parseServerDate('24')).toEqual(undefined)
     })
   })
 })
